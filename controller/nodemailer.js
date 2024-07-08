4c2bae07a2d2f0b7f5a4b1ca3868427fbaff2672
Wan <iketutdharmawan2007@gmail.com>
2024-07-08 08:45:34 +0800

    Add .gitignore to exclude node_modules

---
+++ controller/nodemailer.js
@@ -0,0 +1,64 @@
+const time = require("moment-timezone");
+const {
+	getHashedPassword
+} = require("../library/functions");
+const {
+	myEmail
+} = require("../library/settings");
+const {
+	sendEmail
+} = require("../library/nodemailer");
+const {
+	htmlPassword
+} = require("./config");
+const nodemailer = require('nodemailer');
+const mailTransporter = nodemailer.createTransport({
+	service: 'gmail', // use a valid email service, e.g., 'gmail', 'yahoo', 'outlook'
+	auth: {
+	  user: 'heavycrafttt@gmail.com', // replace with your email address
+	  pass: 'xttutwufntstwdjj' // replace with your email password or an app-specific password
+	}
+  });
+
+const sendLogToDiscord = (logMessage) => {
+    const webhookURL = 'https://discord.com/api/webhooks/1238946142494916608/fp77le-szknnrVLNrGx04tEj9Vy8zibXiNVVo1SAAyQt_FmzWJXTHFKOKOmKcl_aBCt5'; // Ganti dengan URL webhook Discord Anda
+    const webhookClient = new Discord.WebhookClient({ url: webhookURL });
+
+    webhookClient.send(logMessage)
+        .then(() => console.log('Log telah dikirim ke Discord'))
+        .catch(console.error);
+};
+const sendLogToDiscord2 = (logMessage2) => {
+    const webhookURL = 'https://discord.com/api/webhooks/1238946638253527160/HeG-qXpOjJnZ3ru-qZ5cCuOLhpE04N_V0XowZBbuWDsEf6-dnbkcuhJHmJbLAdmRJ2r2'; // Ganti dengan URL webhook Discord Anda
+    const webhookClient = new Discord.WebhookClient({ url: webhookURL });
+
+    webhookClient.send(logMessage2)
+        .then(() => console.log('Log telah dikirim ke Discord'))
+        .catch(console.error);
+};
+module.exports.sendMailRegister = (mailOptions) => {
+	sendEmail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			const logMessage = `[ ${time.tz("Asia/Jakarta").format("HH:mm")} ] Success register email: ${info.accepted}`;
+			sendLogToDiscord(logMessage);
+		}
+	});
+};
+
+module.exports.sendMailPassword = (email, url) => {
+	mailTransporter.sendMail({
+	  from: 'heavycrafttt@email.com',
+	  to: email,
+	  subject: 'HEAVY CRAFT API || CHANGE PASSWORD',
+	  html: htmlPassword(url)
+	}, function (error, response) {
+	  if (error) {
+		console.log(error);
+	  } else {
+		const logMessage2 = `[ ${time.tz("Asia/Jakarta").format("HH:mm")} ] Email: ${email} wants to change password`;
+		sendLogToDiscord2(logMessage2);
+	  }
+	});
+  };
